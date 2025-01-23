0a21f30ccd13c67254221e242d09c9464670808a
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-01-23 16:57:46 -0500

    Added ShimmerComponent to the body

---
+++ src/Components/ShimmerComponent.js
@@ -0,0 +1,20 @@
+const ShimmerComponent = () => {
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+            <div className="shimmer-card">Cards</div>
+        </div>
+    )
+};
+
+export default ShimmerComponent;

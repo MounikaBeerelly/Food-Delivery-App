20aa0134d9b39fdb8921e887d56a4c7932c599ba
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-01-24 16:34:20 -0500

    Added About and contact routes

---
+++ src/Components/AboutComponent.js
@@ -0,0 +1,10 @@
+const AboutComponent = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <h3>This is a React Application</h3>
+        </div>
+    )
+}
+
+export default AboutComponent;
---
+++ src/Components/ContactComponent.js
@@ -0,0 +1,10 @@
+const ContactComponent = () => {
+    return (
+        <div>
+            <h2>Contact Us Page</h2>
+            <h3>This is a Contact component</h3>
+        </div>
+    )
+}
+
+export default ContactComponent;
